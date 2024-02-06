d10a1f491f625a2b1ddd5d452af889c3992ef95e
Jarlbatross <jaraane@gmail.com>
2024-02-06 08:47:36 +0100

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>Home/</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update Ninja One</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age}}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
+
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue';
+
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const ninjaOne = ref({name: 'Mario', age: 30})
+    const ninjaTwo = reactive({name: 'Luigi', age: 35})
+
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40
+    }
+
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 30
+    }
+
+    return {ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo}
+  },
+}
+</script>
