c9b05ccff76739fdecf99093c78d9839de0a2775
Jarlbatross <jaraane@gmail.com>
2024-02-06 10:11:33 +0100

    Update HomeView.vue
    Learning about computed values, watch and watch effect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,41 @@
 <template>
   <div class="home">
-    <h1>Home/</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update Ninja One</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age}}</p>
-    <button @click="updateNinjaTwo">Update Ninja Two</button>
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>Search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">Stop watching</button>
 
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue';
 
 
 export default {
   name: 'HomeView',
   setup() {
-    const ninjaOne = ref({name: 'Mario', age: 30})
-    const ninjaTwo = reactive({name: 'Luigi', age: 35})
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
+    const stopWatch = watch(search, () => {
+      console.log('watch function run');
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log('watcheffect function ran', search.value);
+    }) 
+    
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
     }
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 30
-    }
-
-    return {ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo}
+    return { names, search, matchingNames, handleClick }
   },
 }
 </script>
